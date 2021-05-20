709d8b2decea865c7ba7679fd2ec230a686502fd
Lance Hagen <ldhagen@saconsumer.com>
2021-05-20 17:49:27 -0500

    cleaning up with win10 returns

--- system_file_scan.py
+++ system_file_scan.py
@@ -2,17 +2,25 @@
 import subprocess as sp
 import pickle, os
 
-def main():
+def get_scan():
     print('Hello')
-    p1 = sp.Popen(['find', '/', '-type', 'f'], stdout = sp.PIPE)
+    p1 = sp.Popen(['find', '/var/tmp/ldh/working/system_scanning/', '-type', 'f'], stdout = sp.PIPE)
     out1 = p1.communicate()[0]
     print(len(out1))
     print(type(out1))
+    out2 = str(out1)
     list1 = []
-    for x in out1[:-1].split('/n'):
+    for x in out2[2:-1].split(r'\n'):
         list1.append(x)
     for y in list1:
         print(y)
+    return list1
+
+def main():
+    x = get_scan()
+    print(x[0])
+    with open('output_test1', 'wb') as outw:
+        pickle.dump(x, outw)
 
 if __name__ == '__main__':
      main()
